ff065f422d4ade42c348c60b63dcb3a98dceac66
Michael Zhuang <zhuangdeyouxiang@gmail.com>
2015-11-07 00:44:05 +1100

    Initial Commit

---
+++ myOpenMP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  myOpenMP
+//
+//  Created by Tianhao Zhuang on 7/11/2015.
+//  Copyright © 2015 Tianhao Zhuang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
