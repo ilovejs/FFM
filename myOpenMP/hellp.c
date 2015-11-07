0c1c28baa78abfb7d4cad537a60bec87ca963e50
Michael Zhuang <zhuangdeyouxiang@gmail.com>
2015-11-07 12:30:11 +1100

    compiled ffm-predict

---
+++ myOpenMP/hellp.c
@@ -0,0 +1,16 @@
+//
+//  hellp.c
+//  myOpenMP
+//
+//  Created by Tianhao Zhuang on 7/11/2015.
+//  Copyright © 2015 Tianhao Zhuang. All rights reserved.
+//
+
+//#include "hellp.h"
+//
+//#include <libiomp/omp.h>
+//#include <stdio.h>
+//int main() {
+//#pragma omp parallel
+//    printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
+//}
---
+++ myOpenMP/hellp.h
@@ -0,0 +1,14 @@
+//
+//  hellp.h
+//  myOpenMP
+//
+//  Created by Tianhao Zhuang on 7/11/2015.
+//  Copyright © 2015 Tianhao Zhuang. All rights reserved.
+//
+
+//#ifndef hellp_h
+//#define hellp_h
+//
+//#include <stdio.h>
+//
+//#endif /* hellp_h */
